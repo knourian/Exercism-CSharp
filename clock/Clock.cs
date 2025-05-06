b7110895c9c6711f53367f14f4567c70d3712121
khashayar nourian <knourian.dev@gmail.com>
2025-05-06 18:54:55 +0330

    Add Clock exercise

---
+++ clock/Clock.cs
@@ -0,0 +1,17 @@
+public class Clock
+{
+    public Clock(int hours, int minutes)
+    {
+        throw new NotImplementedException("You need to implement this method.");
+    }
+
+    public Clock Add(int minutesToAdd)
+    {
+        throw new NotImplementedException("You need to implement this method.");
+    }
+
+    public Clock Subtract(int minutesToSubtract)
+    {
+        throw new NotImplementedException("You need to implement this method.");
+    }
+}
