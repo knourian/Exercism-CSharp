0128ab547d5baf7693844b7c934e5231e7ada61d
khashayar nourian <knourian.dev@gmail.com>
2025-05-06 16:51:57 +0330

    Add Pythagorean Triplet exercise

---
+++ pythagorean-triplet/PythagoreanTriplet.cs
@@ -0,0 +1,7 @@
+public static class PythagoreanTriplet
+{
+    public static IEnumerable<(int a, int b, int c)> TripletsWithSum(int sum)
+    {
+        throw new NotImplementedException("You need to implement this method.");
+    }
+}
