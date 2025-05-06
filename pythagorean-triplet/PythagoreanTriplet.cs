8ea84371e38058830335c560f19ebf5c67b41873
khashayar nourian <knourian.dev@gmail.com>
2025-05-06 18:49:29 +0330

    Solverd PythagoreanTriplet

--- pythagorean-triplet/PythagoreanTriplet.cs
+++ pythagorean-triplet/PythagoreanTriplet.cs
@@ -2,6 +2,16 @@ public static class PythagoreanTriplet
 {
     public static IEnumerable<(int a, int b, int c)> TripletsWithSum(int sum)
     {
-        throw new NotImplementedException("You need to implement this method.");
+        for (int a = 1; a < sum / 3; a++)
+        {
+            for (int b = a + 1; b < sum / 2; b++)
+            {
+                int c = sum - a - b;
+                if (a * a + b * b == c * c)
+                {
+                    yield return (a, b, c);
+                }
+            }
+        }
     }
 }
