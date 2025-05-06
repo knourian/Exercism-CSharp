cecb9da93b0a96255d8fc0f044f6fd47d4cc5e6e
khashayar nourian <knourian.dev@gmail.com>
2025-05-06 19:24:17 +0330

    Implement Clock Exercise

--- clock/Clock.cs
+++ clock/Clock.cs
@@ -1,17 +1,58 @@
 public class Clock
 {
+    private int Hours;
+    private int Minutes;
     public Clock(int hours, int minutes)
     {
-        throw new NotImplementedException("You need to implement this method.");
+        SetClock(hours, minutes);
+    }
+
+    private void SetClock(int hours, int minutes)
+    {
+        int totalMinutes = (hours * 60 + minutes) % 1440;
+        if (totalMinutes < 0)
+        {
+            totalMinutes += 1440;
+        }
+        Hours = totalMinutes / 60;
+        Minutes = totalMinutes % 60;
     }
 
     public Clock Add(int minutesToAdd)
     {
-        throw new NotImplementedException("You need to implement this method.");
+        int totalMinutes = (Hours * 60 + Minutes + minutesToAdd) % 1440;
+        Hours = totalMinutes / 60;
+        Minutes = totalMinutes % 60;
+        SetClock(Hours, Minutes);
+        return this;
     }
 
     public Clock Subtract(int minutesToSubtract)
     {
-        throw new NotImplementedException("You need to implement this method.");
+        int totalMinutes = (Hours * 60 + Minutes - minutesToSubtract) % 1440;
+        if (totalMinutes < 0)
+        {
+            totalMinutes += 1440; // Adjust for negative values
+        }
+        Hours = totalMinutes / 60;
+        Minutes = totalMinutes % 60;
+        SetClock(Hours, Minutes);
+        return this;
+    }
+
+    public override string ToString() => $"{Hours:D2}:{Minutes:D2}";
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is null)
+            return false;
+        if (obj.GetType() != GetType())
+            return false;
+        Clock other = (Clock)obj;
+        return Hours == other.Hours && Minutes == other.Minutes;
+    }
+    public override int GetHashCode()
+    {
+        return (Hours * 60 + Minutes).GetHashCode();
     }
 }
