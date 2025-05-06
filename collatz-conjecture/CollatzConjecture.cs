ba49a8b703dc3fde50a23d133335796754aa85bb
khashayar nourian <knourian.dev@gmail.com>
2025-05-06 18:53:04 +0330

    Solved Collatz Conjecture

--- collatz-conjecture/CollatzConjecture.cs
+++ collatz-conjecture/CollatzConjecture.cs
@@ -2,6 +2,20 @@ public static class CollatzConjecture
 {
     public static int Steps(int number)
     {
-        throw new NotImplementedException("You need to implement this function");
+        ArgumentOutOfRangeException.ThrowIfLessThanOrEqual(number, 0);
+        int steps = 0;
+        while (number > 1)
+        {
+            if (number % 2 == 0)
+            {
+                number /= 2;
+            }
+            else
+            {
+                number = number * 3 + 1;
+            }
+            steps++;
+        }
+        return steps;
     }
 }
