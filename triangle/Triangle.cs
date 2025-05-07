d4452c2af676013b2549ea466e46f87ff34282f4
khashayar nourian <knourian.dev@gmail.com>
2025-05-07 16:35:52 +0330

    Solved Triangle exercise

--- triangle/Triangle.cs
+++ triangle/Triangle.cs
@@ -2,16 +2,28 @@ public static class Triangle
 {
     public static bool IsScalene(double side1, double side2, double side3)
     {
-        throw new NotImplementedException("You need to implement this method.");
+        return IsTriangle(side1, side2, side3) && side1 != side2 && side1 != side3 && side2 != side3;
     }
 
-    public static bool IsIsosceles(double side1, double side2, double side3) 
+    public static bool IsIsosceles(double side1, double side2, double side3)
     {
-        throw new NotImplementedException("You need to implement this method.");
+        return IsTriangle(side1, side2, side3) && (side1 == side2 || side1 == side3 || side2 == side3);
     }
 
-    public static bool IsEquilateral(double side1, double side2, double side3) 
+    public static bool IsEquilateral(double side1, double side2, double side3)
     {
-        throw new NotImplementedException("You need to implement this method.");
+        return IsTriangle(side1, side2, side3) && side1 == side2 && side2 == side3;
+    }
+
+    private static bool IsTriangle(double side1, double side2, double side3)
+    {
+        if (side1 < 0 || side2 < 0 || side3 < 0)
+        {
+            return false;
+        }
+
+        return side1 + side2 > side3 &&
+               side1 + side3 > side2 &&
+               side2 + side3 > side1;
     }
 }
