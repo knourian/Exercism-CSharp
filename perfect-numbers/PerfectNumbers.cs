65efdba5178f449855c19ed9a4aaafdae4659fff
khashayar nourian <knourian.dev@gmail.com>
2025-05-08 18:01:12 +0330

    Solved perfect numbers

--- perfect-numbers/PerfectNumbers.cs
+++ perfect-numbers/PerfectNumbers.cs
@@ -9,6 +9,34 @@ public static class PerfectNumbers
 {
     public static Classification Classify(int number)
     {
-        throw new NotImplementedException("You need to implement this method.");
+        ArgumentOutOfRangeException.ThrowIfLessThanOrEqual(number, 0);
+
+        int sumOfDivisors = SumOfDivisors(number);
+
+        if (sumOfDivisors == number)
+        {
+            return Classification.Perfect;
+        }
+        else if (sumOfDivisors > number)
+        {
+            return Classification.Abundant;
+        }
+        else
+        {
+            return Classification.Deficient;
+        }
+    }
+
+    private static int SumOfDivisors(int number)
+    {
+        int sum = 0;
+        for (int i = 1; i <= number / 2; i++)
+        {
+            if (number % i == 0)
+            {
+                sum += i;
+            }
+        }
+        return sum;
     }
 }
