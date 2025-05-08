93487fd86a97a115f59e0069c840550ec7d9c887
khashayar nourian <knourian.dev@gmail.com>
2025-05-08 19:04:49 +0330

    Solved SimpleCipher

--- simple-cipher/SimpleCipher.cs
+++ simple-cipher/SimpleCipher.cs
@@ -1,30 +1,58 @@
+using System.Text;
+
 public class SimpleCipher
 {
+    private string _key;
+    private const string Alphabet = "abcdefghijklmnopqrstuvwxyz";
+    private static readonly Random Rand = new Random();
+
     public SimpleCipher()
     {
-        throw new NotImplementedException("You need to implement this method.");
+        _key = new string(Enumerable.Range(0, 100).Select(x => Alphabet[Rand.Next(Alphabet.Length)]).ToArray());
     }
 
     public SimpleCipher(string key)
     {
-        throw new NotImplementedException("You need to implement this method.");
+        _key = key.ToLower();
     }
-    
-    public string Key 
+
+    public string Key
     {
         get
         {
-            throw new NotImplementedException("You need to implement this method.");
+            return _key;
         }
     }
 
     public string Encode(string plaintext)
     {
-        throw new NotImplementedException("You need to implement this method.");
+        var ciphertext = new StringBuilder(plaintext.Length);
+        for (var i = 0; i < plaintext.Length; i++)
+            ciphertext.Append(EncodeCharacter(plaintext, i));
+        return ciphertext.ToString();
+    }
+
+    private char EncodeCharacter(string plaintext, int idx)
+    {
+        var alphabetIdx = Alphabet.IndexOf(plaintext[idx]) + Alphabet.IndexOf(Key[idx % Key.Length]);
+        if (alphabetIdx >= Alphabet.Length)
+            alphabetIdx -= Alphabet.Length;
+        return Alphabet[alphabetIdx];
     }
 
     public string Decode(string ciphertext)
     {
-        throw new NotImplementedException("You need to implement this method.");
+        var plaintext = new StringBuilder(ciphertext.Length);
+        for (var i = 0; i < ciphertext.Length; i++)
+            plaintext.Append(DecodeCharacter(ciphertext, i));
+        return plaintext.ToString();
+    }
+
+    private char DecodeCharacter(string ciphertext, int idx)
+    {
+        var alphabetIdx = Alphabet.IndexOf(ciphertext[idx]) - Alphabet.IndexOf(Key[idx % Key.Length]);
+        if (alphabetIdx < 0)
+            alphabetIdx += Alphabet.Length;
+        return Alphabet[alphabetIdx];
     }
 }
