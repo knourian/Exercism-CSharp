0a3d1a1b6a99fef8d36315fbe483cddc7af94ab7
khashayar nourian <knourian.dev@gmail.com>
2025-05-08 17:49:11 +0330

    Add Food Chain exercise

---
+++ food-chain/FoodChain.cs
@@ -0,0 +1,12 @@
+public static class FoodChain
+{
+    public static string Recite(int verseNumber)
+    {
+        throw new NotImplementedException("You need to implement this method.");
+    }
+
+    public static string Recite(int startVerse, int endVerse)
+    {
+        throw new NotImplementedException("You need to implement this method.");
+    }
+}
