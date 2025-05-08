b0643394ed17fff0dbc05ddc397c5e7097539a58
khashayar nourian <knourian.dev@gmail.com>
2025-05-08 17:56:53 +0330

    Add Perfect Numbers exercise

---
+++ perfect-numbers/PerfectNumbers.cs
@@ -0,0 +1,14 @@
+public enum Classification
+{
+    Perfect,
+    Abundant,
+    Deficient
+}
+
+public static class PerfectNumbers
+{
+    public static Classification Classify(int number)
+    {
+        throw new NotImplementedException("You need to implement this method.");
+    }
+}
