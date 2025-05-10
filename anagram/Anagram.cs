332a42b0fb2e60d6205529eb0c316922d3585586
khashayar nourian <knourian.dev@gmail.com>
2025-05-10 11:30:38 +0330

    Solved Anagram

--- anagram/Anagram.cs
+++ anagram/Anagram.cs
@@ -1,12 +1,19 @@
 public class Anagram
 {
+    private string _baseWord;
     public Anagram(string baseWord)
     {
-        throw new NotImplementedException("You need to implement this method.");
+        _baseWord = baseWord;
     }
 
     public string[] FindAnagrams(string[] potentialMatches)
     {
-        throw new NotImplementedException("You need to implement this method.");
+        var sortedBaseWord = string.Concat(_baseWord.ToLower().OrderBy(x => x));
+        return potentialMatches
+            .Where(candidate =>
+                !_baseWord.Equals(candidate, StringComparison.OrdinalIgnoreCase) &&
+                candidate.Length == _baseWord.Length &&
+                string.Concat(candidate.ToLower().OrderBy(x => x)) == sortedBaseWord)
+            .ToArray();
     }
 }
