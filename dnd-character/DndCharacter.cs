488f2a3c350e5ed222f5614722e30b58a3bede69
khashayar nourian <knourian.dev@gmail.com>
2025-05-11 15:57:26 +0330

    Solved  D&D character exercise

--- dnd-character/DndCharacter.cs
+++ dnd-character/DndCharacter.cs
@@ -1,5 +1,19 @@
+using System.Security.Cryptography;
+
 public class DndCharacter
 {
+    private DndCharacter(int strength, int dexterity, int constitution, int intelligence, int wisdom, int charisma, int hitpoints)
+    {
+        Strength = strength;
+        Dexterity = dexterity;
+        Constitution = constitution;
+        Intelligence = intelligence;
+        Wisdom = wisdom;
+        Charisma = charisma;
+        Hitpoints = hitpoints;
+    }
+
+
     public int Strength { get; }
     public int Dexterity { get; }
     public int Constitution { get; }
@@ -8,18 +22,32 @@ public class DndCharacter
     public int Charisma { get; }
     public int Hitpoints { get; }
 
-    public static int Modifier(int score)
-    {
-        throw new NotImplementedException("You need to implement this method.");
-    }
+    public static int Modifier(int score) => (int)Math.Floor((score - 10) / 2.0);
 
-    public static int Ability() 
+    public static int Ability()
     {
-        throw new NotImplementedException("You need to implement this method.");
+        var rolls = new int[4];
+        for (var i = 0; i < 4; i++)
+        {
+            rolls[i] = GetDiceRoll();
+        }
+
+        Array.Sort(rolls);
+        return rolls[1] + rolls[2] + rolls[3];
     }
 
     public static DndCharacter Generate()
     {
-        throw new NotImplementedException("You need to implement this method.");
+        var strength = Ability();
+        var dexterity = Ability();
+        var constitution = Ability();
+        var intelligence = Ability();
+        var wisdom = Ability();
+        var charisma = Ability();
+        var hitpoints = 10 + Modifier(constitution);
+        return new DndCharacter(strength, dexterity, constitution, intelligence, wisdom, charisma, hitpoints);
     }
+
+    private static int GetDiceRoll() => RandomNumberGenerator.GetInt32(1, 7);
 }
+
