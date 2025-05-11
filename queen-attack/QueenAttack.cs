acc863c01a2f54bd80c21ffc7d3f53a46bf3a98e
khashayar nourian <knourian.dev@gmail.com>
2025-05-11 16:09:40 +0330

    Solved Queen attack

--- queen-attack/QueenAttack.cs
+++ queen-attack/QueenAttack.cs
@@ -8,17 +8,25 @@ public class Queen
 
     public int Row { get; }
     public int Column { get; }
+
+    public bool CanAttack(Queen other)
+    {
+        if (other == null) throw new ArgumentNullException(nameof(other));
+        return Row == other.Row || Column == other.Column || Math.Abs(Row - other.Row) == Math.Abs(Column - other.Column);
+    }
 }
 
 public static class QueenAttack
 {
-    public static bool CanAttack(Queen white, Queen black)
-    {
-        throw new NotImplementedException("You need to implement this method.");
-    }
+    public static bool CanAttack(Queen white, Queen black) => white.CanAttack(black);
 
     public static Queen Create(int row, int column)
     {
-        throw new NotImplementedException("You need to implement this method.");
+        ArgumentOutOfRangeException.ThrowIfLessThan(row, 0, nameof(row));
+        ArgumentOutOfRangeException.ThrowIfGreaterThanOrEqual(row, 8, nameof(row));
+
+        ArgumentOutOfRangeException.ThrowIfLessThan(column, 0, nameof(column));
+        ArgumentOutOfRangeException.ThrowIfGreaterThanOrEqual(column, 8, nameof(column));
+        return new Queen(row, column);
     }
 }
