b5606df030de21cd57e2a2a0674f58ae7404bda1
khashayar nourian <knourian.dev@gmail.com>
2025-05-11 15:44:13 +0330

    Add D&D character exercise

---
+++ dnd-character/DndCharacter.cs
@@ -0,0 +1,25 @@
+public class DndCharacter
+{
+    public int Strength { get; }
+    public int Dexterity { get; }
+    public int Constitution { get; }
+    public int Intelligence { get; }
+    public int Wisdom { get; }
+    public int Charisma { get; }
+    public int Hitpoints { get; }
+
+    public static int Modifier(int score)
+    {
+        throw new NotImplementedException("You need to implement this method.");
+    }
+
+    public static int Ability() 
+    {
+        throw new NotImplementedException("You need to implement this method.");
+    }
+
+    public static DndCharacter Generate()
+    {
+        throw new NotImplementedException("You need to implement this method.");
+    }
+}
