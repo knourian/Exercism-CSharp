3aa8e561f6f2b3333fb9ce41d1e91a8de3697eb5
khashayar nourian <knourian.dev@gmail.com>
2025-05-11 16:02:31 +0330

    Add Queen Attack exercise

---
+++ queen-attack/QueenAttack.cs
@@ -0,0 +1,24 @@
+public class Queen
+{
+    public Queen(int row, int column)
+    {
+        Row = row;
+        Column = column;
+    }
+
+    public int Row { get; }
+    public int Column { get; }
+}
+
+public static class QueenAttack
+{
+    public static bool CanAttack(Queen white, Queen black)
+    {
+        throw new NotImplementedException("You need to implement this method.");
+    }
+
+    public static Queen Create(int row, int column)
+    {
+        throw new NotImplementedException("You need to implement this method.");
+    }
+}
