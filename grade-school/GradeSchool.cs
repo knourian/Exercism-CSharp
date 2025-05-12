86a120e8119355a5358169ac8fd4d79d8ef8afb7
khashayar nourian <knourian.dev@gmail.com>
2025-05-12 14:39:24 +0330

    Solved Grade School exercise

--- grade-school/GradeSchool.cs
+++ grade-school/GradeSchool.cs
@@ -1,17 +1,9 @@
 public class GradeSchool
 {
-    public bool Add(string student, int grade)
-    {
-        throw new NotImplementedException("You need to implement this method.");
-    }
+    private Dictionary<string, int> _students = [];
+    public bool Add(string student, int grade) => _students.TryAdd(student, grade);
 
-    public IEnumerable<string> Roster()
-    {
-        throw new NotImplementedException("You need to implement this method.");
-    }
+    public IEnumerable<string> Roster() => _students.OrderBy(x => x.Value).ThenBy(x => x.Key).Select(x => x.Key).AsEnumerable();
 
-    public IEnumerable<string> Grade(int grade)
-    {
-        throw new NotImplementedException("You need to implement this method.");
-    }
+    public IEnumerable<string> Grade(int grade) => _students.Where(x => x.Value == grade).OrderBy(x => x.Key).Select(x => x.Key).AsEnumerable();
 }
