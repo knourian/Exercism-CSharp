60e47ab4106bc7f1e192af8ab10d882b16bc8c94
khashayar nourian <knourian.dev@gmail.com>
2025-05-12 14:30:52 +0330

    Added Grade School exercise

---
+++ grade-school/GradeSchool.cs
@@ -0,0 +1,17 @@
+public class GradeSchool
+{
+    public bool Add(string student, int grade)
+    {
+        throw new NotImplementedException("You need to implement this method.");
+    }
+
+    public IEnumerable<string> Roster()
+    {
+        throw new NotImplementedException("You need to implement this method.");
+    }
+
+    public IEnumerable<string> Grade(int grade)
+    {
+        throw new NotImplementedException("You need to implement this method.");
+    }
+}
