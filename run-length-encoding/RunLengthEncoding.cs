813982f654f620a8c6d3bc262a4e05e0445b8009
khashayar nourian <knourian.dev@gmail.com>
2025-05-13 19:56:32 +0330

    Add Run-Length Encoding exercise

---
+++ run-length-encoding/RunLengthEncoding.cs
@@ -0,0 +1,12 @@
+public static class RunLengthEncoding
+{
+    public static string Encode(string input)
+    {
+        throw new NotImplementedException("You need to implement this method.");
+    }
+
+    public static string Decode(string input)
+    {
+        throw new NotImplementedException("You need to implement this method.");
+    }
+}
