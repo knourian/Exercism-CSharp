a25b9145da87994488c6034f954685f72e32941e
khashayar nourian <knourian.dev@gmail.com>
2025-05-17 12:17:20 +0330

    Solved Flatten Array exercise

--- flatten-array/FlattenArray.cs
+++ flatten-array/FlattenArray.cs
@@ -2,8 +2,25 @@ using System.Collections;
 
 public static class FlattenArray
 {
+    // No recursion is used in this implementation.
     public static IEnumerable Flatten(IEnumerable input)
     {
-        throw new NotImplementedException("You need to implement this method.");
+        foreach (var item in input)
+        {
+            if (item is IEnumerable enumerable && !(item is string))
+            {
+                foreach (var subItem in Flatten(enumerable))
+                {
+                    if (subItem != null)
+                    {
+                        yield return subItem;
+                    }
+                }
+            }
+            else if (item != null)
+            {
+                yield return item;
+            }
+        }
     }
 }
