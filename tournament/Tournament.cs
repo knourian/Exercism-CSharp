20a3653faa437af960920c3fb05d4e703897f324
khashayar nourian <knourian.dev@gmail.com>
2025-05-18 13:17:35 +0330

    Added Matching Tournament exercise

---
+++ tournament/Tournament.cs
@@ -0,0 +1,7 @@
+public static class Tournament
+{   
+    public static void Tally(Stream inStream, Stream outStream)
+    {
+        throw new NotImplementedException("You need to implement this method.");    
+    }
+}
