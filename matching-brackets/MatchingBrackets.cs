be040ea9d210c9affa2b6ebdafa2fc993951e746
khashayar nourian <knourian.dev@gmail.com>
2025-05-18 13:10:34 +0330

    Solved Matching Brackets exercise

--- matching-brackets/MatchingBrackets.cs
+++ matching-brackets/MatchingBrackets.cs
@@ -2,6 +2,35 @@ public static class MatchingBrackets
 {
     public static bool IsPaired(string input)
     {
-        throw new NotImplementedException("You need to implement this method.");
+        var stack = new Stack<char>();
+        foreach (var c in input)
+        {
+            if (c == '(' || c == '[' || c == '{')
+            {
+                stack.Push(c);
+            }
+            else if (c == ')' || c == ']' || c == '}')
+            {
+                if (stack.Count == 0)
+                {
+                    return false;
+                }
+
+                var last = stack.Pop();
+                if (IsMatched(last, c) == false)
+                {
+                    return false;
+                }
+            }
+        }
+
+        return stack.Count == 0;
+    }
+
+    private static bool IsMatched(char open, char close)
+    {
+        return (open == '(' && close == ')') ||
+               (open == '[' && close == ']') ||
+               (open == '{' && close == '}');
     }
 }
