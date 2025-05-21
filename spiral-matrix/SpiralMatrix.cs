0cb34f7e6685c7d7485ebcaea5079f569b5ab1af
khashayar nourian <knourian.dev@gmail.com>
2025-05-21 17:38:57 +0330

    Solved spiral-matrix exercise

--- spiral-matrix/SpiralMatrix.cs
+++ spiral-matrix/SpiralMatrix.cs
@@ -2,6 +2,43 @@ public class SpiralMatrix
 {
     public static int[,] GetMatrix(int size)
     {
-        throw new NotImplementedException("You need to implement this method.");
+        int[,] matrix = new int[size, size];
+        int value = 1;
+        int top = 0, bottom = size - 1, left = 0, right = size - 1;
+
+        while (top <= bottom && left <= right)
+        {
+            for (int i = left; i <= right; i++)
+            {
+                matrix[top, i] = value++;
+            }
+            top++;
+
+            for (int i = top; i <= bottom; i++)
+            {
+                matrix[i, right] = value++;
+            }
+            right--;
+
+            if (top <= bottom)
+            {
+                for (int i = right; i >= left; i--)
+                {
+                    matrix[bottom, i] = value++;
+                }
+                bottom--;
+            }
+
+            if (left <= right)
+            {
+                for (int i = bottom; i >= top; i--)
+                {
+                    matrix[i, left] = value++;
+                }
+                left++;
+            }
+        }
+
+        return matrix;
     }
 }
