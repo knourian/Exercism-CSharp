bfdf72d6b7506a60157a967c098b62101ad6b742
khashayar nourian <knourian.dev@gmail.com>
2025-05-24 16:56:41 +0330

    Added Complex Numbers exercise

---
+++ complex-numbers/ComplexNumbers.cs
@@ -0,0 +1,51 @@
+public struct ComplexNumber
+{
+    public ComplexNumber(double real, double imaginary)
+    {
+    }
+
+    public double Real()
+    {
+        throw new NotImplementedException("You need to implement this method.");
+    }
+
+    public double Imaginary()
+    {
+        throw new NotImplementedException("You need to implement this method.");
+    }
+
+    public ComplexNumber Mul(ComplexNumber other)
+    {
+        throw new NotImplementedException("You need to implement this method.");
+    }
+
+    public ComplexNumber Add(ComplexNumber other)
+    {
+        throw new NotImplementedException("You need to implement this method.");
+    }
+
+    public ComplexNumber Sub(ComplexNumber other)
+    {
+        throw new NotImplementedException("You need to implement this method.");
+    }
+
+    public ComplexNumber Div(ComplexNumber other)
+    {
+        throw new NotImplementedException("You need to implement this method.");
+    }
+
+    public double Abs()
+    {
+        throw new NotImplementedException("You need to implement this method.");
+    }
+
+    public ComplexNumber Conjugate()
+    {
+        throw new NotImplementedException("You need to implement this method.");
+    }
+    
+    public ComplexNumber Exp()
+    {
+        throw new NotImplementedException("You need to implement this method.");
+    }
+}
---
+++ complex-numbers/ComplexNumbersTests.cs
@@ -0,0 +1,260 @@
+public class ComplexNumbersTests
+{
+    [Fact]
+    public void Real_part_of_a_purely_real_number()
+    {
+        Assert.Equal(1, new ComplexNumber(1, 0).Real());
+    }
+
+    [Fact]
+    public void Real_part_of_a_purely_imaginary_number()
+    {
+        Assert.Equal(0, new ComplexNumber(0, 1).Real());
+    }
+
+    [Fact]
+    public void Real_part_of_a_number_with_real_and_imaginary_part()
+    {
+        Assert.Equal(1, new ComplexNumber(1, 2).Real());
+    }
+
+    [Fact]
+    public void Imaginary_part_of_a_purely_real_number()
+    {
+        Assert.Equal(0, new ComplexNumber(1, 0).Imaginary());
+    }
+
+    [Fact]
+    public void Imaginary_part_of_a_purely_imaginary_number()
+    {
+        Assert.Equal(1, new ComplexNumber(0, 1).Imaginary());
+    }
+
+    [Fact]
+    public void Imaginary_part_of_a_number_with_real_and_imaginary_part()
+    {
+        Assert.Equal(2, new ComplexNumber(1, 2).Imaginary());
+    }
+
+    [Fact]
+    public void Imaginary_unit()
+    {
+        var actual = new ComplexNumber(0, 1).Mul(new ComplexNumber(0, 1));
+        Assert.Equal(-1, actual.Real());
+        Assert.Equal(0, actual.Imaginary());
+    }
+
+    [Fact]
+    public void Add_purely_real_numbers()
+    {
+        var actual = new ComplexNumber(1, 0).Add(new ComplexNumber(2, 0));
+        Assert.Equal(3, actual.Real());
+        Assert.Equal(0, actual.Imaginary());
+    }
+
+    [Fact]
+    public void Add_purely_imaginary_numbers()
+    {
+        var actual = new ComplexNumber(0, 1).Add(new ComplexNumber(0, 2));
+        Assert.Equal(0, actual.Real());
+        Assert.Equal(3, actual.Imaginary());
+    }
+
+    [Fact]
+    public void Add_numbers_with_real_and_imaginary_part()
+    {
+        var actual = new ComplexNumber(1, 2).Add(new ComplexNumber(3, 4));
+        Assert.Equal(4, actual.Real());
+        Assert.Equal(6, actual.Imaginary());
+    }
+
+    [Fact]
+    public void Subtract_purely_real_numbers()
+    {
+        var actual = new ComplexNumber(1, 0).Sub(new ComplexNumber(2, 0));
+        Assert.Equal(-1, actual.Real());
+        Assert.Equal(0, actual.Imaginary());
+    }
+
+    [Fact]
+    public void Subtract_purely_imaginary_numbers()
+    {
+        var actual = new ComplexNumber(0, 1).Sub(new ComplexNumber(0, 2));
+        Assert.Equal(0, actual.Real());
+        Assert.Equal(-1, actual.Imaginary());
+    }
+
+    [Fact]
+    public void Subtract_numbers_with_real_and_imaginary_part()
+    {
+        var actual = new ComplexNumber(1, 2).Sub(new ComplexNumber(3, 4));
+        Assert.Equal(-2, actual.Real());
+        Assert.Equal(-2, actual.Imaginary());
+    }
+
+    [Fact]
+    public void Multiply_purely_real_numbers()
+    {
+        var actual = new ComplexNumber(1, 0).Mul(new ComplexNumber(2, 0));
+        Assert.Equal(2, actual.Real());
+        Assert.Equal(0, actual.Imaginary());
+    }
+
+    [Fact]
+    public void Multiply_purely_imaginary_numbers()
+    {
+        var actual = new ComplexNumber(0, 1).Mul(new ComplexNumber(0, 2));
+        Assert.Equal(-2, actual.Real());
+        Assert.Equal(0, actual.Imaginary());
+    }
+
+    [Fact]
+    public void Multiply_numbers_with_real_and_imaginary_part()
+    {
+        var actual = new ComplexNumber(1, 2).Mul(new ComplexNumber(3, 4));
+        Assert.Equal(-5, actual.Real());
+        Assert.Equal(10, actual.Imaginary());
+    }
+
+    [Fact]
+    public void Divide_purely_real_numbers()
+    {
+        var actual = new ComplexNumber(1, 0).Div(new ComplexNumber(2, 0));
+        Assert.Equal(0.5, actual.Real());
+        Assert.Equal(0, actual.Imaginary());
+    }
+
+    [Fact]
+    public void Divide_purely_imaginary_numbers()
+    {
+        var actual = new ComplexNumber(0, 1).Div(new ComplexNumber(0, 2));
+        Assert.Equal(0.5, actual.Real());
+        Assert.Equal(0, actual.Imaginary());
+    }
+
+    [Fact]
+    public void Divide_numbers_with_real_and_imaginary_part()
+    {
+        var actual = new ComplexNumber(1, 2).Div(new ComplexNumber(3, 4));
+        Assert.Equal(0.44, actual.Real());
+        Assert.Equal(0.08, actual.Imaginary());
+    }
+
+    [Fact]
+    public void Absolute_value_of_a_positive_purely_real_number()
+    {
+        Assert.Equal(5, new ComplexNumber(5, 0).Abs());
+    }
+
+    [Fact]
+    public void Absolute_value_of_a_negative_purely_real_number()
+    {
+        Assert.Equal(5, new ComplexNumber(-5, 0).Abs());
+    }
+
+    [Fact]
+    public void Absolute_value_of_a_purely_imaginary_number_with_positive_imaginary_part()
+    {
+        Assert.Equal(5, new ComplexNumber(0, 5).Abs());
+    }
+
+    [Fact]
+    public void Absolute_value_of_a_purely_imaginary_number_with_negative_imaginary_part()
+    {
+        Assert.Equal(5, new ComplexNumber(0, -5).Abs());
+    }
+
+    [Fact]
+    public void Absolute_value_of_a_number_with_real_and_imaginary_part()
+    {
+        Assert.Equal(5, new ComplexNumber(3, 4).Abs());
+    }
+
+    [Fact]
+    public void Conjugate_a_purely_real_number()
+    {
+        var actual = new ComplexNumber(5, 0).Conjugate();
+        Assert.Equal(5, actual.Real());
+        Assert.Equal(0, actual.Imaginary());
+    }
+
+    [Fact]
+    public void Conjugate_a_purely_imaginary_number()
+    {
+        var actual = new ComplexNumber(0, 5).Conjugate();
+        Assert.Equal(0, actual.Real());
+        Assert.Equal(-5, actual.Imaginary());
+    }
+
+    [Fact]
+    public void Conjugate_a_number_with_real_and_imaginary_part()
+    {
+        var actual = new ComplexNumber(1, 1).Conjugate();
+        Assert.Equal(1, actual.Real());
+        Assert.Equal(-1, actual.Imaginary());
+    }
+
+    [Fact]
+    public void Euler_s_identity_formula()
+    {
+        var actual = new ComplexNumber(0, Math.PI).Exp();
+        Assert.Equal(-1, actual.Real(), precision: 7);
+        Assert.Equal(0, actual.Imaginary(), precision: 7);
+    }
+
+    [Fact]
+    public void Exponential_of_0()
+    {
+        var actual = new ComplexNumber(0, 0).Exp();
+        Assert.Equal(1, actual.Real(), precision: 7);
+        Assert.Equal(0, actual.Imaginary(), precision: 7);
+    }
+
+    [Fact]
+    public void Exponential_of_a_purely_real_number()
+    {
+        var actual = new ComplexNumber(1, 0).Exp();
+        Assert.Equal(Math.E, actual.Real(), precision: 7);
+        Assert.Equal(0, actual.Imaginary(), precision: 7);
+    }
+
+    [Fact]
+    public void Exponential_of_a_number_with_real_and_imaginary_part()
+    {
+        var actual = new ComplexNumber(Math.Log(2.0), Math.PI).Exp();
+        Assert.Equal(-2, actual.Real(), precision: 7);
+        Assert.Equal(0, actual.Imaginary(), precision: 7);
+    }
+
+    [Fact]
+    public void Exponential_resulting_in_a_number_with_real_and_imaginary_part()
+    {
+        var actual = new ComplexNumber(Math.Log(2.0) / 2, Math.PI / 4).Exp();
+        Assert.Equal(1, actual.Real(), precision: 7);
+        Assert.Equal(1, actual.Imaginary(), precision: 7);
+    }
+
+    [Fact]
+    public void Add_real_number_to_complex_number()
+    {
+        var actual = new ComplexNumber(1, 2).Add(5);
+        Assert.Equal(6, actual.Real());
+        Assert.Equal(2, actual.Imaginary());
+    }
+
+    [Fact]
+    public void Multiply_complex_number_by_real_number()
+    {
+        var actual = new ComplexNumber(2, 5).Mul(5);
+        Assert.Equal(10, actual.Real());
+        Assert.Equal(25, actual.Imaginary());
+    }
+
+    [Fact]
+    public void Divide_complex_number_by_real_number()
+    {
+        var actual = new ComplexNumber(10, 100).Div(10);
+        Assert.Equal(1, actual.Real());
+        Assert.Equal(10, actual.Imaginary());
+    }
+}
