590d32fc12cf52e4e1de8c50f66a1bcdd02b8063
khashayar nourian <knourian.dev@gmail.com>
2025-05-24 16:47:49 +0330

    Solved variable length quantity exercise

--- variable-length-quantity/VariableLengthQuantity.cs
+++ variable-length-quantity/VariableLengthQuantity.cs
@@ -2,11 +2,67 @@ public static class VariableLengthQuantity
 {
     public static uint[] Encode(uint[] numbers)
     {
-        throw new NotImplementedException("You need to implement this method.");
+        if (numbers == null || numbers.Length == 0)
+            return [];
+        var result = new List<uint>();
+
+        foreach (var number in numbers)
+        {
+            var temp = number;
+            var encoded = new List<uint>();
+            while (temp > 0 || encoded.Count == 0)
+            {
+                var byteValue = temp & 0x7F;
+                temp >>= 7;
+                encoded.Add(byteValue);
+            }
+
+            for (int i = encoded.Count - 1; i >= 0; i--)
+            {
+                if (i > 0)
+                {
+                    result.Add(encoded[i] | 0x80);
+                }
+                else
+                {
+                    result.Add(encoded[i]);
+                }
+            }
+        }
+
+        return result.ToArray();
     }
 
     public static uint[] Decode(uint[] bytes)
     {
-        throw new NotImplementedException("You need to implement this method.");
+        if (bytes == null || bytes.Length == 0)
+            return [];
+        var result = new List<uint>();
+        uint currentnumber = 0;
+        int shift = 0;
+
+        for (int i = 0; i < bytes.Length; i++)
+        {
+            var value = bytes[i] & 0x7F;
+            currentnumber = (currentnumber << 7) | value;
+            shift += 7;
+            if ((bytes[i] & 0x80) == 0)
+            {
+                if (shift > 35)
+                {
+                    throw new InvalidOperationException("Decoded value exceeds 32 bits.");
+                }
+                result.Add(currentnumber);
+                currentnumber = 0;
+                shift = 0;
+            }
+            else if (shift >= 35)
+            {
+                throw new InvalidOperationException("Incomplete sequence detected.");
+            }
+
+        }
+
+        return shift > 0 ? throw new InvalidOperationException("Incomplete sequence detected.") : result.ToArray();
     }
 }
